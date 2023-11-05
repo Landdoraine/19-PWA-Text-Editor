cd1e4a672678b6508280b009144949172eefd70f
Landdoraine Chen <wending.chen2020@gmail.com>
2023-11-05 03:09:37 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# PWA Text Editor
+  
+  [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]
+
+## Description
+
+The project is to build a text editor that runs in the browser. The app will be a single-page application that meets the PWA criteria. The application will also function offline.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Credits](#credits)
+
+## Installation
+
+Install NodeJS. Install all dependencies using the command 'npm install' in the terminal. 
+
+## Usage
+
+The app has been deployed on Heroku.
+
+Live URL : 
+
+## License
+
+License: [ISC License (ISC)](https://opensource.org/licenses/ISC)
+
+[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]
+
+
+## Credits
+
+This challenge is done through referencing a provided solution in class.
+
+https://webpack.js.org/guides/getting-started/
---
+++ client/src/js/database.js
@@ -0,0 +1,49 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log('Post to the database');
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.add({ value: content }); // Using auto-incremented ID
+    const result = await request;
+    await tx.done;
+    console.log('Data saved to the database', result);
+  } catch (error) {
+    console.error('Error saving data to the database:', error);
+  }
+};
+
+// Add logic for a method that gets all the content from the database
+export const getDb= async () => {
+  console.log('GET all content from the database');
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = await store.getAll();
+    const result = await request;
+    await tx.done;
+    console.log('result.value', result);
+    return result;
+  } catch (error) {
+    console.error('Error getting data from the database:', error);
+    return [];
+  }
+};
+
+initdb();
